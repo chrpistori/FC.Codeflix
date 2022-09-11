afe53a03b090ef3cd48ca4b77e7dab40f6a4ca96
Christian R. Pistori <chr.pistori@gmail.com>
2022-09-10 21:49:01 -0300

    Iniciando testes de DomainValidation.

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,61 @@
+﻿using Bogus;
+using Xunit;
+using FC.Codeflix.Catalog.Domain.Validation;
+using FluentAssertions;
+using FC.Codeflix.Catalog.Domain.Exceptions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Validation;
+
+public class DomainValidationTest
+{
+    private Faker Faker { get; set; } = new Faker();
+
+    [Fact(DisplayName = nameof(NotNullOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullOk()
+    {
+        var value = Faker.Commerce.ProductName();
+        Action action =
+            () => DomainValidation.NotNull(value, "Value");
+        action.Should().NotThrow();
+    }
+
+
+    [Fact(DisplayName = nameof(NotNullThrowWhenNull))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullThrowWhenNull()
+    {
+        string? value = null;
+        Action action =
+            () => DomainValidation.NotNull(value, "FieldName");
+        action.Should().Throw<EntityValidationException>()
+            .WithMessage("FieldName should not be null");
+    }
+
+
+    [Theory(DisplayName = nameof(NotNullOrEmptyThrowWhenEmpty))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("   ")]
+    public void NotNullOrEmptyThrowWhenEmpty(string? target)
+    {
+        Action action =
+            () => DomainValidation.NotNullOrEmpty(target, "FieldName");
+        action.Should()
+            .Throw<EntityValidationException>()
+            .WithMessage("FieldName should not be null or empty");
+    }
+
+
+    [Fact(DisplayName = nameof(NotNullOrEmptyOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullOrEmptyOk()
+    {
+        var target = Faker.Commerce.ProductName();
+
+        Action action =
+            () => DomainValidation.NotNullOrEmpty(target, "FieldName");
+        action.Should().NotThrow();
+    }
+}
