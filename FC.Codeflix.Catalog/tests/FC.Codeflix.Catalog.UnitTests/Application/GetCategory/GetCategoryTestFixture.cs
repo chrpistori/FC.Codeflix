640bb7c6b39ae0e22531e4e4b63880f54a5cb8f8
Christian R. Pistori <chr.pistori@gmail.com>
2022-09-24 23:22:28 -0300

    Iniciando UseCase GetCategory com o primeiro teste.

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTestFixture.cs
@@ -0,0 +1,44 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.UnitTests.Common;
+using Moq;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.GetCategory;
+
+[CollectionDefinition(nameof(GetCategoryTestFixture))]
+public class GetCategoryTestFixtureCollection : ICollectionFixture<GetCategoryTestFixture> {}
+    
+public class GetCategoryTestFixture : BaseFixture
+{
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = "";
+
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+
+        return categoryDescription;
+    }
+
+    public Category GetValidCategory()
+        => new(
+            GetValidCategoryName(),
+            GetValidCategoryDescription()
+        );
+
+    public Mock<ICategoryRepository> GetRepositoryMock()
+        => new();
+}
